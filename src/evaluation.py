d7c638eafc29d8bf860321385c0680c21067d83f
Haley Lepp <hlepp@uw.edu>
2019-04-17 10:52:19 -0700

    Trying to recover missing code

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,10 +1,9 @@
 #!/opt/python-3.6/bin/python3
 
+import subprocess
 def eval_summary(summaries):
-	# TODO: Loop through summary files and compare to equivalent XML data
-        # TODO: Run ROUGE command within loop
+	subprocess.call([COMMAND])
         # TODO: print to D2_rouge_scores.out
-	print()
 
 if __name__ == '__main__':
 	ROUGE_DATA_DIR = '/dropbox/18-19/573/code/ROUGE/data'
