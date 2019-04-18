2ffdb0e201ee6277cf38bab51445a27a642a3490
Ben Longwill <longwill@uw.edu>
2019-04-18 02:30:51 -0700

    Added document retriever file and comments

--- src/data_input.py
+++ src/data_input.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import nltk
-from lxml import etree
-from lxml import html
+from nltk import sent_tokenize
+from nltk import word_tokenize
 from bs4 import BeautifulSoup
+import document_retriever
 
-#### Put none for non-existing
+#### Put none for non-existing text
 ###############################
 class Topic:
     def __init__(self, topic_id:str=None ,docsetA_id:str=None,  title:str=None , narrative:str=None):
@@ -29,7 +29,19 @@ class Sentence:
         self.tokens=tokens
 
 ###############################
+### Tag Variables used to avoid hardcoding later in the document or in case of tag changes
 
+title_tag='title'
+narrative_tag='narrative'
+docsetA_tag='docseta'
+id_tag='id'
+doc_tag='doc'
+topic_tag='topic'
+parser_tag='lxml'
+
+###############################
+
+# Takes a list of file paths, collects all data and stores into a list of class object 'Topic' data structures
 def get_data(file_paths:list)->list:
 
     task_data = ""
@@ -38,69 +50,39 @@ def get_data(file_paths:list)->list:
 
     return get_topics_list(task_data)
 
-def process_doc_id(doc_id:str):
-    if "_" in doc_id:
-        source, lang, other = doc_id.split("_")
-        date, specifier = other.split(".")
-        year = date[:4]
-    else:
-        source = doc_id[:3]
-        date = doc_id[3:11]
-        year = date[:4]
-        #specifier = doc_id[-4:]
-        lang = "ENG"
-
-    if source == "XIE":
-        alt_source = "XIN"
-    else:
-        alt_source = source
-
-    return source, lang, date, year, alt_source
-
+#Takes Document object and the text from doc file. The block of text is separated into sentences as sentence objects and also tokenized using NLTK.
 def populate_sentence_list(current_doc, doc_text):
 
-    doc_sentences = nltk.sent_tokenize(doc_text)
+    doc_sentences = sent_tokenize(doc_text)
 
     for doc_sentence in doc_sentences:
-        current_doc.sentence_list.append(Sentence(nltk.word_tokenize(doc_sentence)))  ############## Creates sentence object
+        current_doc.sentence_list.append(Sentence(word_tokenize(doc_sentence)))  ############## Creates sentence object
         current_doc.sent_count += 1
+#Takes a Topic class object, an xml or html document set element, and a document retriever object
+# Itterates all document Id's in html/xml element and uses the doc retriever to get the raw document from database
+#Extracts attributes from raw document and creates Document class object, fills it with sentence objects and adds it to the current topic object
+def populate_document_list(current_topic, docsetA, doc_ret:document_retriever.Document_Retriever):
 
-def populate_document_list(current_topic, docsetA, xml_parser_cache):
+    for doc in docsetA.findAll(doc_tag):
 
-    for doc in docsetA.findAll("doc"):
+        doc_id = doc.attrs[id_tag]
 
-        category = None
-        headline = None
-        dateline = None
-        doc_text = None
+        raw_doc = doc_ret.retrieve_doc(doc_id)
+        headline, category, dateline, doc_text = get_doc_attributes(raw_doc, doc_ret.headline_tag, doc_ret.category_tag, doc_ret.dateline_tag, doc_ret.text_tag)
 
-        doc_id = doc.attrs['id']
-        source, lang, date, year, alt_source = process_doc_id(doc_id)
-
-        if int(year) >= 1996 and int(year) <= 2000:
-
-            headline, category, dateline, doc_text = get_acquaint_doc_attributes(doc_id, source, year, date, alt_source,
-                                                                                 xml_parser_cache)
-
-        elif int(year) >= 2004 and int(year) <= 2006:
-
-            headline, category, dateline, doc_text = get_acquaint2_doc_attributes(doc_id, lang, date, alt_source,
-                                                                                  xml_parser_cache)
-
-        current_doc = Document(doc_id=doc_id, headline=headline, dateline=dateline,
-                               category=category)  ########## Creates document object
+        current_doc = Document(doc_id=doc_id, headline=headline, dateline=dateline, category=category)  ########## Creates document object
 
         populate_sentence_list(current_doc, doc_text)
 
         current_topic.document_list.append(current_doc)
-
-
+# Takes the raw topic xml/html and a set of title, narrative, and docset TAGS according to the format of file
+# extracts the respective text including document IDs for Aqcuaint(2) database
 def get_topic_attributes(raw_topic, title_tag:str , narrative_tag:str, docsetA_tag:str):
 
     narrative=None
     title=None
 
-    topic_id = raw_topic.attrs['id']
+    topic_id = raw_topic.attrs[id_tag]
 
     if title_tag:
         title_element = raw_topic.find(title_tag)
@@ -114,43 +96,15 @@ def get_topic_attributes(raw_topic, title_tag:str , narrative_tag:str, docsetA_t
 
     docsetA = raw_topic.find(docsetA_tag)
 
-    docsetA_id = docsetA.attrs['id']
+    docsetA_id = docsetA.attrs[id_tag]
 
     return topic_id, title, narrative, docsetA_id, docsetA
 
-def get_acquaint_doc_attributes(doc_id, source, year, date, alt_source,xml_parser_cache):
-    if alt_source != 'NYT':
-        alt_source = alt_source + "_ENG"
-    doc_path = "/corpora/LDC/LDC02T31/" + source.lower() + "/" + year + "/" + date + "_" + alt_source.upper()
-
-    if doc_path in xml_parser_cache:
-        tree = xml_parser_cache[doc_path]
-    else:
-        parser = etree.HTMLParser(encoding='utf-8', remove_blank_text=True)
-        tree = html.fragment_fromstring(open(doc_path).read(), create_parent='body', parser=parser)
-        xml_parser_cache.update({doc_path: tree})
-
-    raw_doc = [element for element in tree.findall("doc") if element.find("docno").text == " " + doc_id + " "][0]
-
-
-    return get_doc_attributes(raw_doc, 'headline', 'category', 'date_time','text')
-
-def get_acquaint2_doc_attributes(doc_id, lang, date, alt_source,xml_parser_cache):
-    doc_path = "/corpora/LDC/LDC08T25/data/" + alt_source.lower() + "_" + lang.lower() + "/" + alt_source.lower() + "_" + lang.lower() + "_" + date[:6] + ".xml"
-    if doc_path in xml_parser_cache:
-        tree = xml_parser_cache[doc_path]
-    else:
-        tree = etree.parse(doc_path)
-        xml_parser_cache.update({doc_path: tree})
-
-    raw_doc = [element for element in tree.findall("DOC") if element.get("id") == doc_id ][0]
-
-
-    return get_doc_attributes(raw_doc=raw_doc, headline_tag='HEADLINE', dateline_tag='DATELINE', text_tag='TEXT')
-
-
+# Requires raw document and the specific tags used according to current HTML or XML.
+# Extracts the headline category dateline and text if available in raw document.
 def get_doc_attributes(raw_doc, headline_tag:str="", category_tag:str="", dateline_tag:str="", text_tag:str=""):
 
+    ### Instantiate variables in case text not available in raw doc
     headline = None
     category = None
     dateline = None
@@ -174,55 +128,27 @@ def get_doc_attributes(raw_doc, headline_tag:str="", category_tag:str="", dateli
     if text_tag:
         doc_text=' '.join(raw_doc.find(text_tag).itertext())
 
-
     return headline, category, dateline, doc_text
 
-
-
+#Parses Raw data XML file argument and extracts the topic elements.
+# Creates Topic elements and fills withtopic attributes and Document objects
+# Returns these topic objects as a list.
 def get_topics_list(task_data:str)->list:
     topics=[]
-    xml_parser_cache = {}
+    doc_ret = document_retriever.Document_Retriever()
 
-    soup = BeautifulSoup(task_data, "lxml")
-    raw_topics = soup.findAll('topic')
+    soup = BeautifulSoup(task_data, parser_tag)
+    raw_topics = soup.findAll(topic_tag)
 
     for raw_topic in raw_topics:
-        topic_id, title, narrative, docsetA_id, docsetA = get_topic_attributes(raw_topic=raw_topic, title_tag='title', narrative_tag='narrative', docsetA_tag="docseta")
+        topic_id, title, narrative, docsetA_id, docsetA = get_topic_attributes(raw_topic, title_tag, narrative_tag, docsetA_tag)
 
         current_topic= Topic(topic_id=topic_id,title=title,narrative=narrative,docsetA_id=docsetA_id) ########### Creates topic object
 
-        populate_document_list(current_topic, docsetA ,xml_parser_cache)
+        populate_document_list(current_topic, docsetA, doc_ret)
 
         topics.append(current_topic)
 
     return topics
 
 ###############################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
