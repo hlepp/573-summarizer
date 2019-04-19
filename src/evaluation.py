d77358c6ba5356964fbff2c38dfe5133633825ed
Shannon Ladymon <sladymon@uw.edu>
2019-04-19 11:44:45 -0700

    Changed evaluation.py and revised_config.xml to work with paths releative to the main directory (rather than src/) and fixed a few small bugs in them; also changed results/.gitignore to allow files and removed an old .swp file.

--- src/evaluation.py
+++ src/evaluation.py
@@ -5,14 +5,12 @@ from subprocess import Popen, PIPE
 
 def eval_summary():
 	ROUGE_DATA_DIR = '/dropbox/18-19/573/code/ROUGE/data'
-	CONFIG_FILE_WITH_PATH = 'ROUGE/revised_config.xml'
-	COMMAND = './ROUGE/ROUGE-1.5.5.pl -e ' + ROUGE_DATA_DIR + ' -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d ' + CONFIG_FILE_WITH_PATH
+	CONFIG_FILE_WITH_PATH = 'src/ROUGE/revised_config.xml'
+	COMMAND = 'src/ROUGE/ROUGE-1.5.5.pl -e ' + ROUGE_DATA_DIR + ' -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d ' + CONFIG_FILE_WITH_PATH
 	stdout = subprocess.check_output(COMMAND.split())
-	results=open('../results/D2_rouge_scores.out', 'w+')
-	results.write(stdout)
+	results=open('results/D2_rouge_scores.out', 'w+')
+	results.write(stdout.decode())
 	results.close()
-
-eval_summary()
 
 
         
