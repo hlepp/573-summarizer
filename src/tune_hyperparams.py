951b1ce2ae01e5cb2ccaf650be90ae8ff5e272bd
Shannon Ladymon <sladymon@uw.edu>
2019-05-17 10:06:59 -0700

    Completed tuning for all get_data() and select_content() parameters

--- src/tune_hyperparams.py
+++ src/tune_hyperparams.py
@@ -10,8 +10,6 @@ __email__ = 'sladymon@uw.edu'
 from text_summarizer import summarize_text, summarize_topics_list
 from data_input import get_data
 
-import sys  #TODO: Delete
-
 def read_results(output_folder):
     """
     TODO
@@ -31,8 +29,11 @@ def read_results(output_folder):
 
 if __name__ == '__main__':
 
-    # TODO: Test everything
+    # Tune parameters on devtest dataset
     input_path = "/dropbox/18-19/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml" 
+
+
+    ###### Original defaults to start with ######
 
     # D2 defaults
     stemming_default = False
@@ -50,19 +51,8 @@ if __name__ == '__main__':
     mle_lambda_default = 0.6
     k_default = 20
 
-    # TUNED INPUT DATA PARAMETER VALUES
-    lower_tuned = False # TODO: this is True when by itself, but not with stemming
-    stemming_tuned = True
-    idf_type_tuned = "smooth_idf" # Note this is slightly lower for config D2 & 2005, but much better for 2009
-    tf_type_tuned = "term_frequency"
 
-    # TUNED CONTENT SELECTION PARAMETER VALUES
-    d_tuned_dict = {"form_D2": 0.1, "form_2005": 0.2, "form_2009": 0.3}
-    summary_threshold_tuned_dict = {"form_D2": 0.4, "form_2005": 0.3, "form_2009": 0.4}
-    epsilon_tuned_dict = {"form_D2": 0.02, "form_2005": 0.04, "form_2009": 0.1}
-    min_sent_len_tuned_dict = {"form_D2": 3, "form_2005": 3, "form_2009": 4}
-
- 
+    ###### Options for each parameter to test ######
 
     # Test for data input for all configurations
     stemming_opts = [False, True] 
@@ -75,14 +65,37 @@ if __name__ == '__main__':
     summary_threshold_opts =  [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     epsilon_opts = [0.02, 0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20] 
     min_sent_len_opts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    include_narrative = False # TODO: make this an option once added to code
 
     # Test only for cosine (form_D2, form_2005)
+    include_narrative_opts = [False, True]
     intersent_threshold_opts =  [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
     # Test only for gen/norm (form_2009)
     mle_lambda_opts =  [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    k_opts = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+    k_opts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+
+    ###### TUNED VALUES ######
+
+    # TUNED INPUT DATA PARAMETER VALUES
+    stemming_tuned = True
+    lower_tuned = False 
+    idf_type_tuned = "smooth_idf" # Note this is slightly lower for config D2 & 2005, but much better for 2009
+    tf_type_tuned = "term_frequency"
+
+    # TUNED CONTENT SELECTION PARAMETER VALUES
+    d_tuned_dict = {"form_D2": 0.1, "form_2005": 0.2, "form_2009": 0.3}
+    summary_threshold_tuned_dict = {"form_D2": 0.4, "form_2005": 0.3, "form_2009": 0.4}
+    epsilon_tuned_dict = {"form_D2": 0.02, "form_2005": 0.04, "form_2009": 0.1}
+    min_sent_len_tuned_dict = {"form_D2": 3, "form_2005": 3, "form_2009": 4}
+
+    # TUNED CONFIG_D2 and CONFIG_2005 PARAMETER VALUES
+    include_narrative_tuned = False
+    intersent_threshold_tuned_dict = {"form_D2": 0.0, "form_2005": 0.1, "form_2009": 0.0} 
+
+    # TUNED CONFIG_2009 PARAMETER VALUES
+    mle_lambda_tuned = 0.6
+    k_tuned = 9
 
     ###### Get the Data #####
 
@@ -106,13 +119,14 @@ if __name__ == '__main__':
         summary_threshold_tuned = summary_threshold_tuned_dict[form]
         epsilon_tuned = epsilon_tuned_dict[form]
         min_sent_len_tuned = min_sent_len_tuned_dict[form]
+        intersent_threshold_tuned = intersent_threshold_tuned_dict[form]
 
-        # TODO: Tune narrative next, after it is added
-        for min_sent_val in min_sent_len_opts:
+        # Run all values to test
+        for k_val in k_opts:
             
-            output_folder = "D3_tune_" + form + "_min_sent_" + str(min_sent_val)
+            output_folder = "D3_tune_" + form + "_k_" + str(k_val)
 
-            summarize_topics_list(topics, output_folder, d_tuned, intersent_threshold_default, summary_threshold_tuned, epsilon_tuned, mle_lambda_default, k_default, min_sent_len_tuned, include_narrative_default, bias_formula, intersent_formula)
+            summarize_topics_list(topics, output_folder, d_tuned, intersent_threshold_tuned, summary_threshold_tuned, epsilon_tuned, mle_lambda_tuned, k_tuned, min_sent_len_tuned, include_narrative_tuned, bias_formula, intersent_formula)
 
             read_results(output_folder)
 
