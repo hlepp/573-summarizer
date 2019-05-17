1b854f0638760fc8ae50b6b2ea97152bf2ffa2aa
Shannon Ladymon <sladymon@uw.edu>
2019-05-16 18:06:30 -0700

    Added draft of read_results() to tune_hyperparams.py

--- src/tune_hyperparams.py
+++ src/tune_hyperparams.py
@@ -7,17 +7,30 @@ __author__ = 'Shannon Ladymon'
 __email__ = 'sladymon@uw.edu'
 
 
-#from text_summarizer import summarize_text
+from text_summarizer import summarize_text, summarize_topics_list
+from data_input import get_data
 
-# TODO: Change text_summarizer to have a method, which is called by its main
-# (rather than everything in main as it is now
-# Possibly also rename to "nutshell.py"??
+import time
+
+
+def read_results(output_folder):
+    """
+    TODO
+    """
+    results_file = 'results/' + output_folder + '_rouge_scores.out'
+
+    with open(results_file) as f:
+        for line in f:
+            if "ROUGE-2 Average_R:" in line:  # TODO: Do we care about R-1 scores?
+                split_line = line.split()
+                r2_val = float(split_line[3])
+                print("For output_folder={}, ROUGE-2 val={}".format(output_folder, r2_val))
 
 
 if __name__ == '__main__':
 
     # TODO: Test everything
-    input_path = "/dropbox/18-19/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml" # TODO:add train/eval as options
+    input_path = "/dropbox/18-19/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml" 
 
     # D2 defaults
     stemming_default = False
@@ -54,6 +67,15 @@ if __name__ == '__main__':
     mle_lambda_opts =  [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     k_opts = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 
+    start_time = time.time()
+    ###### Get the Data #####
+    topics_lower_false = get_data(input_path, output_folder, stemming=default, lower=False, idf_type=idf_type_default, tf_type=tf_type_default)
+
+    # TODO: once everything is tested, run this as well
+#    topics_lower = get_data(input_path, output_folder, stemming=default, lower=True, idf_type=idf_type_default, tf_type=tf_type_default)
+
+    get_data_time = time.time()
+
     # Run tests for each of the three formula configurations
 
     ###### form_D2 - what we ran in D2 ######
@@ -61,12 +83,18 @@ if __name__ == '__main__':
     bias_formula = "cos"
     intersent_formula = "cos"
 
-    # Tune only stemming
-    for val in stemming_opts:
-        output_folder = "D3_tune_" + form + "_stemming_" + str(val)
+    output_folder = "D3_tune_" + form + "_lower_False"
 
-        # TODO: update this after seeing what other params people add
-#        summarize_text(input_path, output_folder, stemming=val, lower=lower_default, idf_type=idf_type_default, , tf_type=tf_type_default, d=d_default, intersent_threshold=intersent_threshold_default, summary_threshold=summary_threshold_default, epsilon=epsilon_default, mle_lambda=mle_lambda_default, k=k_default, min_sent_len=min_sent_len_default, include_narrative=include_narrative_default, bias_formula, intersent_formula)
+    summarize_topics_list(topics_lower_false, output_folder, d=d_default, intersent_threshold=intersent_threshold_default, summary_threshold=summary_threshold_default, epsilon=epsilon_default, mle_lambda=mle_lambda_default, k=k_default, min_sent_len=min_sent_len_default, include_narrative=include_narrative_default, bias_formula, intersent_formula)
+
+    read_results(output_folder)
+
+    form_D2_time = time.time()
+
+    # Tune only stemming
+#    for val in stemming_opts:
+#        output_folder = "D3_tune_" + form + "_stemming_" + str(val)
+
 
 
     ###### form_2005 - the formulas from the Otterbacher 2005 paper ######
@@ -74,16 +102,30 @@ if __name__ == '__main__':
     bias_formula = "rel"
     intersent_formula = "cos"
 
-    # Tune only stemming
-    for val in stemming_opts:
 
+    output_folder = "D3_tune_" + form + "_lower_False"
+
+    summarize_topics_list(topics_lower_false, output_folder, d=d_default, intersent_threshold=intersent_threshold_default, summary_threshold=summary_threshold_default, epsilon=epsilon_default, mle_lambda=mle_lambda_default, k=k_default, min_sent_len=min_sent_len_default, include_narrative=include_narrative_default, bias_formula, intersent_formula)
+
+    read_results(output_folder)
+
+
+    form_2005_time = time.time()
 
     ###### form_2009 - the formulas from the Otterbacher 2009 paper ######
     form = "form_2009"
     bias_formula = "gen"
     intersent_formula = "norm"
 
-    # Tune only stemming
-    for val in stemming_opts:
-        output_folder = "D3_tune_" + form + "_stemming_" + str(val)
 
+    output_folder = "D3_tune_" + form + "_lower_False"
+
+    summarize_topics_list(topics_lower_false, output_folder, d=d_default, intersent_threshold=intersent_threshold_default, summary_threshold=summary_threshold_default, epsilon=epsilon_default, mle_lambda=mle_lambda_default, k=k_default, min_sent_len=min_sent_len_default, include_narrative=include_narrative_default, bias_formula, intersent_formula)
+
+
+    read_results(output_folder)
+
+    form_2009_time = time.time()
+
+
+    print("Time to get data = {}\nTime to do form_D2={}\nTime to do form_2005={}\nTime to do form_2009={}\nTOTAL TIME={}".format(get_data_time - start_time, form_D2_time - get_data_time, form_2005_time - form_D2_time, form_2005_time - form_2009_time, time.time() - start_time))
