bbcc7bb12596735089bc3408564a0532e26c14b5
Shannon Ladymon <sladymon@uw.edu>
2019-05-16 23:00:52 -0700

    Includes tuning runs for everything before narratie

--- src/tune_hyperparams.py
+++ src/tune_hyperparams.py
@@ -50,17 +50,32 @@ if __name__ == '__main__':
     mle_lambda_default = 0.6
     k_default = 20
 
+    # TUNED INPUT DATA PARAMETER VALUES
+    lower_tuned = False # TODO: this is True when by itself, but not with stemming
+    stemming_tuned = True
+    idf_type_tuned = "smooth_idf" # Note this is slightly lower for config D2 & 2005, but much better for 2009
+    tf_type_tuned = "term_frequency"
 
-    # Test for all formulas
+    # TUNED CONTENT SELECTION PARAMETER VALUES
+    d_tuned_dict = {"form_D2": 0.1, "form_2005": 0.2, "form_2009": 0.3}
+    summary_threshold_tuned_dict = {"form_D2": 0.4, "form_2005": 0.3, "form_2009": 0.4}
+    epsilon_tuned_dict = {"form_D2": 0.02, "form_2005": 0.04, "form_2009": 0.1}
+    min_sent_len_tuned_dict = {"form_D2": 3, "form_2005": 3, "form_2009": 4}
+
+ 
+
+    # Test for data input for all configurations
     stemming_opts = [False, True] 
     lower_opts = [False, True] 
-    include_narrative = False # TODO: make this an option once added to code
     idf_type_opts = ["smooth_idf", "probabilistic_idf", "standard_idf"]
     tf_type_opts = ["term_frequency", "log_normalization"]
+
+    # Test for content selection for all configurations
     d_opts = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     summary_threshold_opts =  [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     epsilon_opts = [0.02, 0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20] 
     min_sent_len_opts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    include_narrative = False # TODO: make this an option once added to code
 
     # Test only for cosine (form_D2, form_2005)
     intersent_threshold_opts =  [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
@@ -71,11 +86,8 @@ if __name__ == '__main__':
 
     ###### Get the Data #####
 
-    lower_false = False
-    topics_lower_false = get_data(input_path, stemming_default, lower_false, idf_type_default, tf_type_default)
+    topics = get_data(input_path, stemming_tuned, lower_tuned, idf_type_tuned, tf_type_tuned)
 
-    lower_true = True
-    topics_lower_true = get_data(input_path, stemming_default, lower_true, idf_type_default, tf_type_default)
 
     # Run tests for each of the three formula configurations
 
@@ -89,23 +101,20 @@ if __name__ == '__main__':
         bias_formula = config[1] 
         intersent_formula = config[2]
 
+        # Get params tuned for each configuration
+        d_tuned = d_tuned_dict[form]
+        summary_threshold_tuned = summary_threshold_tuned_dict[form]
+        epsilon_tuned = epsilon_tuned_dict[form]
+        min_sent_len_tuned = min_sent_len_tuned_dict[form]
 
-        output_folder = "D3_tune_" + form + "_lower_false"
+        # TODO: Tune narrative next, after it is added
+        for min_sent_val in min_sent_len_opts:
+            
+            output_folder = "D3_tune_" + form + "_min_sent_" + str(min_sent_val)
 
-        summarize_topics_list(topics_lower_false, output_folder, d_default, intersent_threshold_default, summary_threshold_default, epsilon_default, mle_lambda_default, k_default, min_sent_len_default, include_narrative_default, bias_formula, intersent_formula)
+            summarize_topics_list(topics, output_folder, d_tuned, intersent_threshold_default, summary_threshold_tuned, epsilon_tuned, mle_lambda_default, k_default, min_sent_len_tuned, include_narrative_default, bias_formula, intersent_formula)
 
-        read_results(output_folder)
-
-        output_folder = "D3_tune_" + form + "_lower_true"
-
-        summarize_topics_list(topics_lower_true, output_folder, d_default, intersent_threshold_default, summary_threshold_default, epsilon_default, mle_lambda_default, k_default, min_sent_len_default, include_narrative_default, bias_formula, intersent_formula)
-
-        read_results(output_folder)
+            read_results(output_folder)
 
         print()  # Print newline to separate
-
-    # Tune only stemming
-#    for val in stemming_opts:
-#        output_folder = "D3_tune_" + form + "_stemming_" + str(val)
-
 
